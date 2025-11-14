37722d2a939a5998f0857e8af3fc643d38ad28bf
Vojtěch Pavelka <xpavel41@stud.fit.vutbr.cz>
2025-11-14 18:29:02 +0100

    Made import endpoint accept file instead of JSON input

--- shared/dto/ImportInputDto.ts
+++ shared/dto/ImportInputDto.ts
@@ -1,23 +1,21 @@
 export type ImportInputDto = {
-    invoices: {
-        id: string
-        invoicedWeight: number
-        invoicedPrice: number,
-        shipment: {
+    id: string
+    invoicedWeight: number
+    invoicedPrice: number,
+    shipment: {
+        id: string,
+        createdAt: string,
+        trackingNumber: string,
+        provider: string,
+        mode: string,
+        originCountry: string,
+        destinationCountry: string,
+        company: {
             id: string,
-            createdAt: string,
-            trackingNumber: string,
-            provider: string,
-            mode: string,
-            originCountry: string,
-            destinationCountry: string,
-            company: {
-                id: string,
-                name: string
-            }
+            name: string
         }
-    }[];
-}
+    }
+}[]
 
 export type ImportResponseDto = {
     success: boolean;
